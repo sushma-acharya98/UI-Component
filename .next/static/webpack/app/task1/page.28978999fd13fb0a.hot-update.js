c2cb6863734fbac5741e8beb8688cfcecd6c9c6d
Sushma Acharya <sushmaacharya345@gmail.com>
2025-07-12 22:28:30 +0545

    Add code banner

---
+++ .next/static/webpack/app/task1/page.28978999fd13fb0a.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/task1/page",{
+
+/***/ "(app-pages-browser)/./src/components/testimonialBanner/testimonialbanner.css":
+/*!****************************************************************!*\
+  !*** ./src/components/testimonialBanner/testimonialbanner.css ***!
+  \****************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"f253b24136d6\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9jb21wb25lbnRzL3Rlc3RpbW9uaWFsQmFubmVyL3Rlc3RpbW9uaWFsYmFubmVyLmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIi9Vc2Vycy9yYW1lc2gvRGVza3RvcC9zdXNobWFqcy9WaXJpdC10YXNrQi9teS1jb2RlLWNsb25lLXRhc2t2aXJpdC9zcmMvY29tcG9uZW50cy90ZXN0aW1vbmlhbEJhbm5lci90ZXN0aW1vbmlhbGJhbm5lci5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJmMjUzYjI0MTM2ZDZcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/components/testimonialBanner/testimonialbanner.css\n"));
+
+/***/ })
+
+});
--- src/components/testimonialBanner/page.js
+++ src/components/testimonialBanner/page.js
@@ -2,33 +2,49 @@
 import "./testimonialbanner.css";
 
 const testimonials = [
-    { id: 1, img: '/images/user1.jpg', alt: 'User 1' },
-    { id: 2, img: '/images/user2.jpg', alt: 'User 2' },
-    { id: 3, img: '/images/user3.jpg', alt: 'User 3' },
-    { id: 4, img: '/images/user4.jpg', alt: 'User 4' },
-    { id: 5, img: '/images/user5.jpg', alt: 'User 5' },
-    { id: 6, img: '/images/user6.jpg', alt: 'User 6' },
-    { id: 7, img: '/images/user7.jpg', alt: 'User 7' }
+    { id: 1, img: '/banner/user1.png', alt: 'User 1' },
+    { id: 2, img: '/banner/thumb.gif', alt: 'User 1' },
+    { id: 3, img: '/banner/user2.png', alt: 'User 2' },
+    { id: 4, img: '/banner/user3.png', alt: 'User 3' },
+    { id: 5, img: '/banner/like.gif', alt: 'User 1' },
+    { id: 6, img: '/banner/user4.png', alt: 'User 4' },
+    { id: 7, img: '/banner/award.gif', alt: 'User 1' },
+    { id: 8, img: '/banner/user5.png', alt: 'User 5' },
+    { id: 9, img: '/banner/smile.gif', alt: 'User 1' },
+    { id: 10, img: '/banner/user6.png', alt: 'User 6' },
 ];
 
 const Page = () => {
     return (
-        <div className="testimonial-wrapper">
-            <div className="testimonial-circle">
-                {testimonials.map((user) => (
-                <div className="testimonial-avatar" key={user.id}>
-                    <img src={user.img} alt={user.alt} />
+        <div className="block_testimonial">
+            <div className='container'>
+                <div className='testimonial_outer'>
+
+                    <div className="testimonial_circle">
+                        {testimonials.map((user, index) => (
+                            <div
+                            className="testimonial-avatar"
+                            key={user.id}
+                            data-user={index + 1}
+                            >
+                            <img src={user.img} alt={user.alt} />
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="testimonial_text">
+                        <p>Hear How They Level Up Their Game!</p>
+                        <h2>
+                            Skill <span className="highlight">Masters</span> Unite! <span>🤝</span>
+                        </h2>
+                        <a href="#" className="testimonial_link">
+                            View all Testimonials <span className="arrow">
+                            <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.34171 0.508396C7.79732 0.0527842 8.53601 0.052784 8.99162 0.508396L13.6583 5.17506C14.1139 5.63067 14.1139 6.36937 13.6583 6.82498L8.99162 11.4916C8.53601 11.9473 7.79732 11.9473 7.34171 11.4916C6.8861 11.036 6.8861 10.2973 7.34171 9.84173L10.0168 7.16669L1.16667 7.16669C0.522335 7.16669 -2.81646e-08 6.64435 0 6.00002C2.81647e-08 5.35569 0.522335 4.83335 1.16667 4.83335H10.0168L7.34171 2.15831C6.8861 1.7027 6.8861 0.964008 7.34171 0.508396Z" fill="#2B2B2B"/></svg>
+                            </span>
+                        </a>
+                    </div>
                 </div>
-                ))}
-            </div>
-            <div className="testimonial-text">
-                <p>Hear How They Level Up Their Game!</p>
-                <h2>
-                Skill <span className="highlight">Masters</span> Unite! <span>🤝</span>
-                </h2>
-                <a href="#" className="testimonial-link">
-                View all Testimonials <span className="arrow">→</span>
-                </a>
             </div>
         </div>
     )
